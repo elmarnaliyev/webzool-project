6a374805ecae8e6b6284997ec0511231022166a0
Elmar Aliyev <elmarnaliyev@gmail.com>
2018-12-10 22:34:14 +0400

    Login required

--- main/views.py
+++ main/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render, redirect
 from django.contrib import messages, auth
 from main.models import User
 from django.contrib import auth
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
+@login_required(login_url='/login')
 def index(request):
     return render(request, 'pages/index.html')
 
